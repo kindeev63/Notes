62c129653f85c266109ba6c0bc54006384825311
Kindeev <kindeevaleksandr163@mail.ru>
2023-06-04 14:31:03 +0400

    add task to database

--- app/src/main/java/com/kindeev/notes/db/NoteDatabase.kt
+++ app/src/main/java/com/kindeev/notes/db/NoteDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [Note::class, Category::class, Reminder::class], version = 1)
+@Database(entities = [Note::class, Category::class, Reminder::class, Task::class], version = 1)
 abstract class NoteDataBase: RoomDatabase(){
     abstract fun getDao(): NoteDao
     companion object {
