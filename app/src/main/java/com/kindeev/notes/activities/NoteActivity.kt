8f415862c638d10c9f8c959243095b505a9578e9
Kindeev <kindeevaleksandr163@mail.ru>
2023-07-06 12:22:37 +0400

    add ViewModel to NoteActivity

--- app/src/main/java/com/kindeev/notes/activities/NoteActivity.kt
+++ app/src/main/java/com/kindeev/notes/activities/NoteActivity.kt
@@ -5,90 +5,44 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import android.view.View
-import android.view.ViewGroup
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.LinearLayout
-import android.widget.Toast
-import androidx.appcompat.app.AlertDialog
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatDelegate
 import com.kindeev.notes.other.MainApp
-import com.kindeev.notes.viewmodels.MainViewModel
 import com.kindeev.notes.R
 import com.kindeev.notes.other.States
 import com.kindeev.notes.databinding.ActivityNoteBinding
-import com.kindeev.notes.db.Note
 import com.kindeev.notes.fragments.ReminderDialogFragment
 import com.kindeev.notes.other.Colors
-import java.util.*
-import kotlin.collections.ArrayList
+import com.kindeev.notes.viewmodels.NoteActivityViewModel
 
 class NoteActivity : AppCompatActivity() {
+    private val viewModel: NoteActivityViewModel by viewModels()
     private lateinit var binding: ActivityNoteBinding
-    private var categoriesList: ArrayList<String> = arrayListOf()
-    lateinit var mainViewModel: MainViewModel
-    private lateinit var currentNote: Note
-    private var save = true
-    private var color: Int = -1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityNoteBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // Отключение автоматического включения темной темы
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
 
         States.noteEdited = true
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        mainViewModel = (application as MainApp).mainViewModel
-        setData()
-        setSpinnerAdapter()
-    }
-
-    private fun createDialog() {
-        val categoriesNames: Array<String> =
-            (mainViewModel.allCategoriesOfNotes.value ?: emptyList()).toList().map { it.name }
-                .toTypedArray()
-        val checkedCategories =
-            categoriesNames.map { it in categoriesList }.toBooleanArray()
-
-
-        val builder = AlertDialog.Builder(this)
-        val chosenCategories = ArrayList(categoriesList)
-        builder.setTitle(resources.getString(R.string.select_categories))
-        builder.setMultiChoiceItems(
-            categoriesNames,
-            checkedCategories
-        ) { _, index, isChecked ->
-            checkedCategories[index] = isChecked
-            if (checkedCategories[index]) {
-                if (categoriesNames[index] !in chosenCategories) chosenCategories.add(
-                    categoriesNames[index]
-                )
-            } else chosenCategories.remove(categoriesNames[index])
-        }
-        builder.setPositiveButton(resources.getString(R.string.save)) { _, _ ->
-            categoriesList = chosenCategories
-            saveNote()
-        }
-        builder.setNegativeButton(resources.getString(R.string.cancel)) { d, _ -> d.cancel() }
-        builder.create().show()
-    }
-
-    private fun setData() {
-        mainViewModel.getNoteById(intent.getIntExtra("noteId", 0)) { oldNote ->
-            if (oldNote == null) notSaveFinish()
-            currentNote = oldNote!!.copy()
-            color = oldNote.color
+        val noteId = intent.getIntExtra("noteId", 0)
+        viewModel.getNoteById(
+            noteId = noteId, mainViewModel = mainViewModel()
+        ) {
             binding.apply {
-                eNoteTitle.setText(oldNote.title)
-                eNoteText.setText(oldNote.text)
-            }
-            binding.colorPickerNote.setSelection(Colors.colors.indexOf(color))
-            if (oldNote.categories.isNotEmpty()) {
-                categoriesList = ArrayList(oldNote.categories.split(", "))
+                eNoteTitle.setText(viewModel.note?.title)
+                eNoteText.setText(viewModel.note?.text)
+                binding.colorPickerNote.adapter =
+                    viewModel.getSpinnerAdapter(this@NoteActivity, layoutInflater)
+                binding.colorPickerNote.onItemSelectedListener = viewModel.spinnerItemSelected()
+                binding.colorPickerNote.setSelection(
+                    Colors.colors.indexOf(
+                        viewModel.note?.color ?: Color.WHITE
+                    )
+                )
             }
         }
     }
@@ -96,22 +50,23 @@ class NoteActivity : AppCompatActivity() {
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.note_menu, menu)
         menu?.findItem(R.id.set_category_item)?.isVisible =
-            (mainViewModel.allCategoriesOfNotes.value?.size
-                ?: 0) > 0
+            (mainViewModel().allCategoriesOfNotes.value?.size ?: 0) > 0
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
-            R.id.set_category_item -> createDialog()
+            R.id.set_category_item -> viewModel.showCategoriesPickerDialog(mainViewModel(), this)
             android.R.id.home -> finish()
             R.id.add_reminder_item -> {
-                saveNote { note ->
-                    val dialogFragment =
-                        ReminderDialogFragment.newInstance(
-                            reminder = null,
-                            noteId = note.id
-                        )
+                viewModel.saveNote(
+                    mainViewModel = mainViewModel(),
+                    title = binding.eNoteTitle.text.toString(),
+                    text = binding.eNoteText.text.toString()
+                ) { note ->
+                    val dialogFragment = ReminderDialogFragment.newInstance(
+                        reminder = null, noteId = note.id
+                    )
                     dialogFragment.show(supportFragmentManager, "reminder_dialog_tag")
                 }
             }
@@ -120,72 +75,15 @@ class NoteActivity : AppCompatActivity() {
 
     }
 
-    private fun notSaveFinish() {
-        save = false
-        Toast.makeText(this, R.string.note_not_exist, Toast.LENGTH_SHORT).show()
-        finish()
-    }
-
     override fun onDestroy() {
         super.onDestroy()
-        if (save) saveNote()
+        viewModel.saveNote(
+            mainViewModel = mainViewModel(),
+            title = binding.eNoteTitle.text.toString(),
+            text = binding.eNoteText.text.toString()
+        )
         States.noteEdited = false
     }
 
-    private fun saveNote(function: (Note) -> Unit = {}) {
-        val noteTitle = binding.eNoteTitle.text.toString()
-        val noteText = binding.eNoteText.text.toString()
-        val noteCategories = categoriesList.joinToString(separator = ", ")
-        currentNote.title = noteTitle
-        currentNote.text = noteText
-        currentNote.categories = noteCategories
-        currentNote.time = Calendar.getInstance().timeInMillis
-        currentNote.color = color
-        mainViewModel.insertNote(note = currentNote, function)
-    }
-
-    private fun setSpinnerAdapter() {
-
-
-        val colorAdapter = object : ArrayAdapter<Int>(this, R.layout.spinner_item, Colors.colors) {
-            override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-                val view: View =
-                    convertView ?: layoutInflater.inflate(R.layout.spinner_item, parent, false)
-                val colorItem = view.findViewById<LinearLayout>(R.id.colorItem)
-                val color = getItem(position)
-                colorItem.setBackgroundColor(color ?: Color.TRANSPARENT)
-                return view
-            }
-
-            override fun getDropDownView(
-                position: Int,
-                convertView: View?,
-                parent: ViewGroup
-            ): View {
-                val view: View =
-                    convertView ?: layoutInflater.inflate(R.layout.spinner_item, parent, false)
-                val colorItem = view.findViewById<LinearLayout>(R.id.colorItem)
-                val color = getItem(position)
-                colorItem.setBackgroundColor(color ?: Color.TRANSPARENT)
-                return view
-            }
-        }
-        binding.colorPickerNote.adapter = colorAdapter
-        binding.colorPickerNote.onItemSelectedListener =
-            object : AdapterView.OnItemSelectedListener {
-                override fun onItemSelected(
-                    parent: AdapterView<*>,
-                    view: View?,
-                    position: Int,
-                    id: Long
-                ) {
-                    val newColor = parent.getItemAtPosition(position) as Int
-                    color = newColor
-                }
-
-                override fun onNothingSelected(parent: AdapterView<*>) {
-                    // Ничего не делаем
-                }
-            }
-    }
+    fun mainViewModel() = (application as MainApp).mainViewModel
 }
---
+++ app/src/main/java/com/kindeev/notes/viewmodels/NoteActivityViewModel.kt
@@ -0,0 +1,119 @@
+package com.kindeev.notes.viewmodels
+
+import android.content.Context
+import android.graphics.Color
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.LinearLayout
+import androidx.appcompat.app.AlertDialog
+import androidx.lifecycle.ViewModel
+import com.kindeev.notes.R
+import com.kindeev.notes.db.Note
+import com.kindeev.notes.other.Colors
+import java.util.Calendar
+import java.util.Date
+
+class NoteActivityViewModel : ViewModel() {
+    var note: Note? = null
+        set(value) {
+            field = value
+            value?.categories?.let {
+                categoriesList = ArrayList(it.split(", "))
+            }
+        }
+    var categoriesList = arrayListOf<String>()
+
+    fun getNoteById(noteId: Int, mainViewModel: MainViewModel, function: () -> Unit) {
+        mainViewModel.getNoteById(noteId) { oldNote ->
+            note = oldNote ?: createNote(mainViewModel.allNotes.value ?: emptyList())
+            function()
+        }
+    }
+
+    private fun createNote(notes: List<Note>): Note {
+        val idsList = notes.map { it.id }
+        var noteId = 0
+        while (true) {
+            if (noteId !in idsList) break
+            noteId++
+        }
+        val currentDate = Date()
+        return Note(noteId, "", "", "", currentDate.time, Color.WHITE)
+    }
+
+    fun getSpinnerAdapter(context: Context, layoutInflater: LayoutInflater) =
+        object : ArrayAdapter<Int>(context, R.layout.spinner_item, Colors.colors) {
+            override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+                val view: View =
+                    convertView ?: layoutInflater.inflate(R.layout.spinner_item, parent, false)
+                val colorItem = view.findViewById<LinearLayout>(R.id.colorItem)
+                val color = getItem(position)
+                colorItem.setBackgroundColor(color ?: Color.TRANSPARENT)
+                return view
+            }
+
+            override fun getDropDownView(
+                position: Int, convertView: View?, parent: ViewGroup
+            ): View {
+                val view: View =
+                    convertView ?: layoutInflater.inflate(R.layout.spinner_item, parent, false)
+                val colorItem = view.findViewById<LinearLayout>(R.id.colorItem)
+                val color = getItem(position)
+                colorItem.setBackgroundColor(color ?: Color.TRANSPARENT)
+                return view
+            }
+        }
+
+    fun spinnerItemSelected() = object : AdapterView.OnItemSelectedListener {
+        override fun onItemSelected(
+            parent: AdapterView<*>, view: View?, position: Int, id: Long
+        ) {
+            val color = parent.getItemAtPosition(position) as Int
+            note?.color = color
+        }
+
+        override fun onNothingSelected(parent: AdapterView<*>) {}
+    }
+
+    fun showCategoriesPickerDialog(mainViewModel: MainViewModel, context: Context) {
+        val categoriesNames: Array<String> =
+            (mainViewModel.allCategoriesOfNotes.value ?: emptyList()).toList().map { it.name }
+                .toTypedArray()
+        val checkedCategories = categoriesNames.map { it in categoriesList }.toBooleanArray()
+
+
+        val builder = AlertDialog.Builder(context)
+        val chosenCategories = ArrayList(categoriesList)
+        builder.setTitle(R.string.select_categories)
+        builder.setMultiChoiceItems(
+            categoriesNames, checkedCategories
+        ) { _, index, isChecked ->
+            checkedCategories[index] = isChecked
+            if (checkedCategories[index]) {
+                if (categoriesNames[index] !in chosenCategories) chosenCategories.add(
+                    categoriesNames[index]
+                )
+            } else chosenCategories.remove(categoriesNames[index])
+        }
+        builder.setPositiveButton(R.string.save) { _, _ ->
+            categoriesList = chosenCategories
+        }
+        builder.setNegativeButton(R.string.cancel) { d, _ -> d.cancel() }
+        builder.create().show()
+    }
+
+    fun saveNote(
+        mainViewModel: MainViewModel, title: String, text: String, function: (Note) -> Unit = {}
+    ) {
+        note?.let { note ->
+            mainViewModel.insertNote(
+                note = note.copy(
+                    title = title, text = text, time = Calendar.getInstance().timeInMillis
+                ), function = function
+            )
+        }
+    }
+}
